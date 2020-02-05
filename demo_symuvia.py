1d56fdecbef1b1d48e9cc54aea5ea153dde56a50
Andres Ladino <aladinoster@gmail.com>
2020-02-05 13:51:21 +0100

    :sparkles: Adding script symuvia
    This scripts defines basic functionalities to call the symuvia shared library in python.

---
+++ demo_symuvia.py
@@ -0,0 +1,31 @@
+# Input: simulator path (Relative/Absolute)
+simulator_path = "libSymuVia.dylib"
+
+# Input: Scenario (Network + Demand + Control)
+scenario_file = "scenario.xml"
+
+from ctypes import cdll, c_int, by_ref, c_bool, create_string_buffer
+
+# Charge library into python 
+symuvia = cdll.LoadLibrary(simulator_path)
+
+# Create an instance for the simulation (0 = success)
+status = symuvia.SymLoadNetworkEx(simulator_path.encode('UTF8'))
+
+# Generate buffers
+sRequest = create_string_buffer(10000) # Data queries
+bEnd = c_int() # Boolean to stop simululation
+bTrace = c_bool(False) # Store traces in XML
+
+# Step by step
+bContinue = True
+while bContinue:
+    bSuccess  = symuvia.SymRunNextStepEx(sRequest, bTrace, byref(bEnd));
+    data = sRequest.value # Retrieve info right after executing step
+
+# other methods 
+# symuvia.SymCreateVehicleEx # Create a vehicle of a specific predefined type
+# symuvia.SymDriveVehicleEx # Modifies position of a vehicle (longitudinal/lateral)
+
+
+
