ab7b1b2d73748e6f4a00665cd90e2224f972c493
Andres Ladino <aladinoster@gmail.com>
2020-02-24 11:50:49 +0100

    :sparkles: Adding configurator
    There is now a configurator file on the command line interface to store some default values relevant at the moment of runtime

--- ensemble/cli.py
+++ ensemble/cli.py
@@ -6,30 +6,111 @@
 
 import sys
 import click
+import typing
 
-from .ensemble import launch_simulation
+from .ensemble import launch_simulation, check_scenario_consistancy
+
+# ------------------------------ Configurator ----------------------------------
+class Configurator(object):
+    """ 
+        This class stores some simulation configurations. 
+    """
+
+    def __init__(self, verbose: bool = False) -> None:
+        import platform
+
+        self.verbose = verbose
+        self.platform = platform.system()
+        self.simulation_platform = ""
+        self.scenario_files = []
+        self.library_path = []
+
+    def set_simulation_platform(self, simulation_platform: str = "") -> None:
+        """ A simpler setter for the simulation platform based on OS
+
+        :param simulation_platform: "symuvia" or "vissim", defaults to ""
+        :type simulation_platform: str, optional
+        """
+        if simulation_platform:
+            self.simulation_platform = simulation_platform
+            return
+        dct_simp = {
+            "Darwin": "symuvia",
+            "Linux": "symuvia",
+            "Windows": "vissim",
+        }
+        self.simulation_platform = dct_simp.get(self.platform, "")
+        return
+
+
+pass_config = click.make_pass_decorator(Configurator)
+
+# ------------------------------ CLI interface ---------------------------------
 
 
 @click.group()
-def main(args=None):
+@click.option("--verbose", is_flag=True)
+@click.option("--platform", default="", help="'symuvia' or 'vissim'")
+@click.pass_context
+def main(ctx, verbose: bool, platform: str) -> int:
     """Scenario launcher for ENSEMBLE simulations"""
-    click.echo(
-        "Replace this message by putting your code into " "ensemble.cli.main"
-    )
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    ctx.obj = Configurator(verbose)
+    ctx.obj.set_simulation_platform(platform)
+    if ctx.obj.verbose:
+        click.echo(
+            """ENSEMBLE Platooning
+
+               Platform for Simulation of Multibrand Truck Platooning
+            """
+        )
+        click.echo(
+            """See click documentation at:
+
+            """
+        )
     return 0
 
 
+# Launch command
 @main.command()
-# @click.option(
-#     "-s", "--scenario", default="", help="Scenario file under analysis."
-# )
-# @click.option("-l", "--library", default="", help="Full path towards library.")
-def launch():  # scenario, library_path):
-    """ Launch a given scenario in a specific platform 
+@click.option(
+    "-s",
+    "--scenario",
+    default="",
+    multiple=True,
+    help="Scenario file(s) under analysis.",
+)
+@click.option("-l", "--library", default="", help="Full path towards library.")
+@pass_config
+def launch(config: Configurator, scenario: str, library: str) -> None:
+    """ Launches an escenario for a specific platform 
     """
-    click.echo("Launching Scenario")
-    launch_simulation()
+    click.echo(
+        "Launching Scenario on platform: "
+        + click.style((f"{config.platform}"), fg="green")
+    )
+    config.library_path = library
+    config.scenario_files = scenario
+    launch_simulation(config)
+
+
+# Check command
+@main.command()
+@click.option(
+    "-s",
+    "--scenario",
+    default="",
+    multiple=True,
+    help="Scenario file under analysis.",
+)
+@click.option("-l", "--library", default="", help="Full path towards library.")
+@pass_config
+def check(config: Configurator, scenario: str, library: str) -> None:
+    """ Diagnoses files consistancy and simulator availability
+    """
+    config.library_path = library
+    config.scenario_files = scenario
+    check_scenario_consistancy(config)
 
 
 if __name__ == "__main__":
