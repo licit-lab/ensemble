d77e78dc6862d73bbf1e8f923c43db35656c93df
Andres Ladino <aladinoster@gmail.com>
2020-03-03 18:32:29 +0100

    🚧 WIP Objects for tactical layer

---
+++ ensemble/control/tactical.py
@@ -0,0 +1,13 @@
+"""
+    This module contains objects that implement the tactical control layer and communication towards the operational layer
+"""
+
+
+class MultiBrandPlatoonServer(object):
+    def __init__(self):
+        pass
+
+
+class MultiBrandPlatoonClient(object):
+    def __init__(self):
+        pass
