279644dca7e7e72212b0906393767ffc3dd421f1
Andres Ladino <aladinoster@gmail.com>
2020-03-04 10:01:33 +0100

    ✨ Adding new error exceptions

--- ensemble/tools/exceptions.py
+++ ensemble/tools/exceptions.py
@@ -5,6 +5,32 @@
 import warnings, click
 
 
+class EnsembleAPIError(Exception):
+    def __init__(self, error_message: str, target_dir: str = "") -> None:
+        super().__init__(error_message)
+        self._target_dir = target_dir
+
+    @property
+    def target_dir(self) -> str:
+        return self._target_dir
+
+    @property
+    def get_messsage(self) -> str:
+        (mess,) = self.args
+        return mess
+
+
+class EnsembleAPILoadLibraryError(EnsembleAPIError):
+    def __init__(self, error_message: str, target_dir: str = "") -> None:
+        super().__init__(error_message, target_dir)
+
+    def __str__(self) -> str:
+        return f"{self.get_messsage}, at: {self._target_dir}"
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.get_messsage},{self._target_dir})"
+
+
 class EnsembleAPIWarning(Exception):
     """ General Warnining
     """
