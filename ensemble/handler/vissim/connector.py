b50004ba2e22f13b286075a281a8ca399026750e
Andres Ladino <andres.ladino@ifsttar.fr>
2020-03-09 11:15:57 +0100

    Revert "Added Vissim Scenario Object"
    This reverts commit c8caf4d6978d6a727078cc13af90bc685836a3e7

--- ensemble/handler/vissim/connector.py
+++ ensemble/handler/vissim/connector.py
@@ -4,9 +4,6 @@
 
 
 import click
-from pathlib import Path
-from ensemble.input.scenario import Scenario
-from ensemble.tools.exceptions import EnsembleAPIWarning, EnsembleAPILoadFileError
 
 try:
     import win32com.client as com
@@ -14,55 +11,6 @@ except ModuleNotFoundError:
     click.echo(click.style("\t Platform non compatible with Windows", fg="yellow"))
 
 from ensemble.tools.exceptions import EnsembleAPILoadLibraryError
-
-
-class ScenarioVissim(Scenario):
-
-    @classmethod
-    def create_vissim_input(cls, *args):
-        """ Looks for indicated vissim scenario paths and performs validation create the scenario"""
-
-        existing_files = [file for file in args if Path(file).exists()]
-
-        # Filter
-        find_inpx = lambda files: [x for x in files if Path(x).suffix == '.inpx']
-        find_layx = lambda files: [x for x in files if Path(x).suffix == '.layx']
-        find_csv = lambda files: [x for x in files if Path(x).suffix == '.csv']
-
-        if existing_files:
-            # Takes first element by default
-            try:
-                inpx_path = find_inpx(existing_files)[0]
-            except IndexError:
-                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an INPX file")
-            try:
-                layx_path = find_layx(existing_files)[0]
-            except IndexError:
-                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an LAYX file")
-            try:
-                platooncsv_path = find_csv(existing_files)[0]
-            except IndexError:
-                EnsembleAPIWarning(f"\tNo Platoon information provided.")
-                platooncsv_path = None
-
-            return cls(inpx_path, layx_path, platooncsv_path)
-        raise EnsembleAPILoadFileError(f"Provided files are not found", args)
-
-    @property
-    def filename(self):
-        """ Vissim property shortcut"""
-        return self.scn_file
-
-    @property
-    def filename_layx(self):
-        """ Vissim property shortcut"""
-        return self.layout_file
-
-    @property
-    def filename_encoded(self):
-        """ Symuvia property shortcut for loading"""
-        return self.scn_file.encode("UTF8")
-
 
 
 class VissimConnector(object):
@@ -87,16 +35,3 @@ class VissimConnector(object):
         except OSError:
             raise EnsembleAPILoadLibraryError("Library not found", self._path)
         self._library = lib_vissim
-
-    def load_scenario(self, scenario):
-        """ checks existance and load scenario into
-        """
-        if isinstance(scenario, ScenarioVissim):
-            try:
-                self._library.LoadNet(self.filename)
-                self._library.LoadLayout(self.filename_layx)
-                return
-            except:
-                raise EnsembleAPILoadFileError(f"\t Simulation could not be loaded")
-                raise click.Abort()
-        EnsembleAPIWarning(f"\tSimulation could not be loaded.")
