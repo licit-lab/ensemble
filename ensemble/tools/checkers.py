dabeda47f5cbbe3d5521b302e38afb7244521a29
Andres Ladino <aladinoster@gmail.com>
2020-03-11 08:35:30 +0100

    🎨 Identing outputs when loading for better reading

--- ensemble/tools/checkers.py
+++ ensemble/tools/checkers.py
@@ -16,16 +16,16 @@ from pathlib import Path
 def check_library_path(configurator) -> bool:
     """ Returns true if platform is available
     """
-    click.echo("Looking for library path: " + click.style(f"{configurator.library_path}", fg="blue"))
+    click.echo("\tLooking for library path: " + click.style(f"{configurator.library_path}", fg="blue"))
 
     # Check Path existance for symuvia
     if configurator.simulation_platform == "symuvia" and Path(configurator.library_path).exists():
         return True
     elif configurator.simulation_platform == "symuvia":
-        click.echo("Given Library Path: " + click.style(f"{configurator.library_path}", fg="red") + " does not exist")
+        click.echo("\tGiven Library Path: " + click.style(f"{configurator.library_path}", fg="red") + " does not exist")
     else:
         click.echo(
-            f"Platform and path: "
+            f"\tPlatform and path: "
             + click.style(f"{configurator.platform} -- {configurator.library_path}", fg="red",)
             + " were not verified."
         )
@@ -35,25 +35,25 @@ def check_library_path(configurator) -> bool:
 def check_scenario_path(configurator) -> bool:
     """ Returns true if all scenario file(s) are available
     """
-    click.echo("Looking for scenario files: " + click.style(f"{configurator.scenario_files}", fg="blue"))
+    click.echo("\tLooking for scenario files: " + click.style(f"{configurator.scenario_files}", fg="blue"))
     scenario = True
     if configurator.scenario_files:
         for file in configurator.scenario_files:
             if Path(file).exists():
-                click.echo("Input File: " + click.style(f"{file} Found", fg="green", bold=True))
+                click.echo("\tInput File: " + click.style(f"{file} Found", fg="green", bold=True))
                 temp = True
             else:
                 temp = False
                 EnsembleAPIWarning(f"Input File: {file}. Not Found ")
         return scenario and temp
-    raise click.UsageError("Scenario file(s) is an empty list")
+    raise click.UsageError("\tScenario file(s) is an empty list")
 
 
 def check_scenario_consistency(configurator) -> bool:
     """ Determine the consistency of files and libraries where indicated"""
 
     # Print info
-    click.echo(click.style("Checking consistency of files", fg="blue", bold=True))
+    click.echo(click.style("\tChecking consistency of files", fg="blue", bold=True))
 
     # Check for Library
     platform = check_library_path(configurator)
