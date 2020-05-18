4a54349963f86faabbd91cb0b3f1eb891f32949a
Adjenughwure <kingsley.adjenughwure@tno.nl>
2020-05-19 00:06:11 +0200

    Vissim Handler Updated to include query data

---
+++ ensemble/handler/vissim/connectorCopy.py
@@ -0,0 +1,118 @@
+"""
+    This module contains objects for modeling a simplified connector to handle vissim
+"""
+
+
+import click
+from pathlib import Path
+from ensemble.input.scenario import Scenario
+from ensemble.tools.exceptions import (
+    EnsembleAPIWarning,
+    EnsembleAPILoadFileError,
+    EnsembleAPILoadLibraryError,
+)
+
+try:
+    import win32com.client as com
+    from pywintypes import com_error
+except ModuleNotFoundError:
+    click.echo(click.style("\t Platform non compatible with Windows", fg="yellow"))
+
+
+class VissimScenario(Scenario):
+    """ 
+        Scenario class for Vissim
+    """
+
+    def __init__(self, *args):
+        self.bread_additional = False
+        super().__init__(*args)
+
+    @classmethod
+    def create_input(cls, *args):
+        """ Looks for indicated vissim scenario paths and performs validation create the scenario"""
+
+        existing_files = [file for file in args if Path(file).exists()]
+
+        # Filter
+        find_inpx = lambda files: [x for x in files if Path(x).suffix == ".inpx"]
+        find_layx = lambda files: [x for x in files if Path(x).suffix == ".layx"]
+        find_csv = lambda files: [x for x in files if Path(x).suffix == ".csv"]
+
+        if existing_files:
+            # Takes first element by default
+            try:
+                inpx_path = find_inpx(existing_files)[0]
+            except IndexError:
+                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an INPX file")
+            try:
+                layx_path = find_layx(existing_files)[0]
+            except IndexError:
+                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an LAYX file")
+            try:
+                platooncsv_path = find_csv(existing_files)[0]
+            except IndexError:
+                EnsembleAPIWarning(f"\tNo Platoon information provided.")
+                platooncsv_path = None
+
+            return cls(inpx_path, layx_path, platooncsv_path)
+        raise EnsembleAPILoadFileError(f"Provided files are not found", args)
+
+    @property
+    def filename(self):
+        """ Vissim property shortcut"""
+        return self.scn_file
+
+    @property
+    def filename_layx(self):
+        """ Vissim property shortcut"""
+        return self.layout_file
+
+    @property
+    def filename_encoded(self):
+        """ Symuvia property shortcut for loading"""
+        return self.scn_file.encode("UTF8")
+
+
+class VissimConnector(object):
+    """ 
+        This models a connector and interactions from the API with the Vissim library. 
+
+        :raises EnsembleAPILoadLibraryError: Raises error when library cannot be loaded
+    """
+
+    def __init__(self, path: str) -> None:
+        self._path = path  # "Vissim.Vissim-64.10"# path
+        self.load_vissim()
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.libraryname})"
+
+    def load_vissim(self) -> None:
+        """ load Vissim COM interface"""
+        try:
+            lib_vissim = com.gencache.EnsureDispatch(self._path)
+            click.echo(click.style(f"\t Library successfully loaded!", fg="green", bold=True))
+        except OSError:
+            raise EnsembleAPILoadLibraryError("Library not found", self._path)
+        except com_error:
+            click.echo(click.style(f"\t Visssim is currently unavailable!", fg="red", bold=True))
+            lib_vissim = None
+            raise EnsembleAPILoadLibraryError("Library not found", self._path)
+        self._library = lib_vissim
+
+    def load_scenario(self, scenario):
+        """ checks existance and load scenario into 
+        """
+        if isinstance(scenario, ScenarioVissim):
+            try:
+                self._library.LoadNet(scenario.filename, scenario.bread_additional)
+                return
+            except:
+                raise EnsembleAPILoadFileError(f"\t Simulation network could not be loaded")
+            try:
+                self._library.LoadLayout(scenario.filename_layx)
+                return
+            except:
+                raise EnsembleAPILoadFileError(f"\t Simulation layout could not be loaded")
+        EnsembleAPIWarning(f"\tSimulation could not be loaded.")
---
+++ ensemble/handler/vissim/scenario.py
@@ -0,0 +1,161 @@
+"""
+**Scenario Module**
+
+    This module contains descriptions that stablish a traffic scenario. A traffic scenario for SymuVia  is regularly described by a simulation object that points towards properties of the simulation file in this case an XML.
+"""
+
+# ============================================================================
+# STANDARD  IMPORTS
+# ============================================================================
+from pathlib import Path
+from datetime import datetime
+import os
+from lxml import etree
+
+# ============================================================================
+# INTERNAL IMPORTS
+# ============================================================================
+from ensemble.input.scenario import Scenario
+
+from ensemble.tools.exceptions import (
+    EnsembleAPIWarning,
+    EnsembleAPILoadFileError,
+    EnsembleAPILoadLibraryError,
+)
+
+from ensemble.tools import constants as ct
+
+
+# ============================================================================
+# CLASS AND DEFINITIONS
+# ============================================================================
+
+
+class VissimScenario(Scenario):
+    """
+           Scenario class for Vissim
+       """
+
+    def __init__(self, *args):
+        self.bread_additional = False
+        super().__init__(*args)
+
+    @classmethod
+    def create_input(cls, *args):
+        """ Looks for indicated vissim scenario paths and performs validation create the scenario"""
+
+        existing_files = [file for file in args if Path(file).exists()]
+
+        # Filter
+        find_inpx = lambda files: [x for x in files if Path(x).suffix == ".inpx"]
+        find_layx = lambda files: [x for x in files if Path(x).suffix == ".layx"]
+        find_csv = lambda files: [x for x in files if Path(x).suffix == ".csv"]
+
+        if existing_files:
+            # Takes first element by default
+            try:
+                inpx_path = find_inpx(existing_files)[0]
+            except IndexError:
+                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an INPX file")
+            try:
+                layx_path = find_layx(existing_files)[0]
+            except IndexError:
+                raise EnsembleAPILoadFileError(f"\tProvided files do not match expected input. Provide an LAYX file")
+            try:
+                platooncsv_path = find_csv(existing_files)[0]
+            except IndexError:
+                EnsembleAPIWarning(f"\tNo Platoon information provided.")
+                platooncsv_path = None
+
+            return cls(inpx_path, layx_path, platooncsv_path)
+        raise EnsembleAPILoadFileError(f"Provided files are not found", args)
+
+    @property
+    def filename(self):
+        """ Vissim property shortcut"""
+        return self.scn_file
+
+    @property
+    def filename_layx(self):
+        """ Vissim property shortcut"""
+        return self.layout_file
+
+    @property
+    def filename_encoded(self):
+        """ Symuvia property shortcut for loading"""
+        return self.scn_file.encode("UTF8")
+
+    def load_xml_tree(self) -> None:
+        """ Load XML file_name
+        """
+        # TODO: Add validation with DTD
+        tree = etree.parse(self.filename())
+        root = tree.getroot()
+        self.xmltree = root
+
+    def get_simulation_parameters(self) -> tuple:
+        """ Get simulation parameters
+
+        :return: tuple with XML dictionary containing parameters
+        :rtype: tuple
+        """
+        branch_tree = "SIMULATIONS"
+        sim_params = self.xmltree.xpath(branch_tree)[0].getchildren()
+        return tuple(par.attrib for par in sim_params)
+
+    def get_vehicletype_information(self) -> tuple:
+        """ Get the vehicle parameters
+
+        :return: tuple of dictionaries containing vehicle parameters
+        :rtype: tuple
+        """
+        branch_tree = "TRAFICS/TRAFIC/TYPES_DE_VEHICULE"
+        vehicle_types = self.xmltree.xpath(branch_tree)[0].getchildren()
+        return tuple(v.attrib for v in vehicle_types)
+
+    def get_network_endpoints(self) -> tuple:
+        """ Get networks endpoint names
+
+        :return: tuple containing endpoint names
+        :rtype: tuple
+        """
+        branch_tree = "TRAFICS/TRAFIC/EXTREMITES"
+        end_points = self.xmltree.xpath(branch_tree)[0].getchildren()
+        return tuple(ep.attrib["id"] for ep in end_points)
+
+    def get_network_links(self) -> tuple:
+        """ Get network link names
+
+        :return: tuple containing link names
+        :rtype: tuple
+        """
+        branch_tree = "TRAFICS/TRAFIC/TRONCONS"
+        links = self.xmltree.xpath(branch_tree)[0].getchildren()
+        return tuple(ep.attrib["id"] for ep in links)
+
+    def get_simulation_steps(self, simid: int = 0) -> range:
+        """Get simulation steps for an simulation. specify the simulation id  via an integer value
+
+        :param simid: simulation id , defaults to 0
+        :type simid: int, optional
+        :return:
+        :rtype: range
+        """
+        t1 = datetime.strptime(self.get_simulation_parameters()[simid].get("debut"), ct.HOUR_FORMAT)
+        t2 = datetime.strptime(self.get_simulation_parameters()[simid].get("fin"), ct.HOUR_FORMAT)
+        t = t2 - t1
+        n = t.seconds / float(self.get_simulation_parameters()[simid].get("pasdetemps"))
+        return range(int(n))
+
+    def filename(self, encoding: str = None):
+        """
+            This method returns the value of encoding of the simulation scenario under consideration
+
+            :param encoding: enconder UTF8, defaults to None
+            :type encoding: string, optional
+            :return: Full path of scenario
+            :rtype: string
+        """
+        if encoding == "UTF8":
+            return self.scn_file.encode(encoding)
+        return self.scn_file
