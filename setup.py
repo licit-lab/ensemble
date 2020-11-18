d10afd4a0116bb4b4e09a1c21549438efbed94ce
Andres Ladino <aladinoster@gmail.com>
2020-11-18 09:00:34 +0100

    ⚙️  Dependencies on setup.py

--- setup.py
+++ setup.py
@@ -20,19 +20,37 @@ requirements = [
     "python-decouple>=3.3",
 ]
 
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
+
+dev_requirements = [
+    "sphinx==3.2.1",
+    "recommonmark==0.6.0",
+    "pytest==6.1.1",
+    "bump2version==1.0.0",
+    "twine==3.2.0",
+    "black==19.10b0",
+    "pylint==2.6.0",
+    "sphinx-rtd-theme==0.5.0",
+    "tox==3.20.0",
+    "coverage==5.3",
+    "flake8==3.8.4",
+]
 
 setup(
     author="Andres Ladino",
     author_email="andres.ladino@ifsttar.fr",
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
     description="A command line interface to launch scenarios for the ENSEMBLE project ",
@@ -44,7 +62,10 @@ setup(
     name="ensemble",
     packages=find_packages(include=["ensemble", "ensemble.*"]),
     test_suite="tests",
-    url="https://github.com/andres.ladino-ifsttar/ensemble",
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    extra_require={"dev": dev_requirements},    
+    url="https://ci.tno.nl/gitlab/paco.hamers-tno/ensemble_drivermodel",
     version="0.1.0",
     zip_safe=False,
 )
