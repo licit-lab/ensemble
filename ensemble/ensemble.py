09f5df04d4d9be62c71f897411f3d83a47897ade
Andres Ladino <aladinoster@gmail.com>
2021-07-29 10:57:44 +0200

    🎨 Code aesthetics

--- ensemble/ensemble.py
+++ ensemble/ensemble.py
@@ -12,41 +12,48 @@ import click
 # ============================================================================
 # INTERNAL IMPORTS
 # ============================================================================
-
+from ensemble.configurator import Configurator
 from ensemble.tools.screen import log_in_terminal
 from ensemble.tools.checkers import check_scenario_consistency
 from ensemble.logic import RuntimeDevice
 from ensemble.control.operational.basic_test import runtime_op_layer
 
+# ============================================================================
+# CLASS AND DEFINITIONS
+# ============================================================================
 
-def launch_simulation(configurator):
-    """ Launches a RuntimeDevice starting from a configurator 
 
-        Args:
-            configurator(Configurator): Configurator class
+def launch_simulation(configurator: Configurator):
+    """Launches a RuntimeDevice starting from a configurator
 
-        Example:
-            Use the the function from a python package as::
-                >>> from ensemble import launch_simulation, Configurator
-                >>> c = Configurator() # default arguments
-                >>> library = 'path/to/simulator'
-                >>> scenarios = ('path/to/scenario',) 
-                >>> config.update_values(library_path=library, scenario_files=scenario)
-                >>> launch_simulation(configurator)
+    Args:
+        configurator {Configurator} -- Configurator file
+
+    Example:
+        Use the the function from a python package as::
+            >>> from ensemble import launch_simulation, Configurator
+            >>> c = Configurator() # default arguments
+            >>> library = 'path/to/simulator'
+            >>> scenarios = ('path/to/scenario',)
+            >>> config.update_values(library_path=library, scenario_files=scenario)
+            >>> launch_simulation(configurator)
     """
     log_in_terminal("Initializing scenario ⏱", fg="magenta")
 
-    with RuntimeDevice(configurator) as rt:
+    with RuntimeDevice(configurator):
         log_in_terminal("Finalizing simulation ⏱", fg="magenta")
 
 
-def check_consistency(configurator) -> bool:
-    """ Checks consistency of the current configurator
-    
-        Args:
-            configurator(Configurator): Configurator class
+def check_consistency(configurator: Configurator) -> bool:
+    """Checks consistency of the current configurator
+
+    Arguments:
+        configurator {Configurator} -- Configurator file
+
+    Returns:
+        bool -- True in case all is correct
     """
-    # Check only for consistency, others could be added in future
+    # Check only for platform/file consistency, others could be added in future
     return check_scenario_consistency(configurator)
 
 
