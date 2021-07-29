f47b1dc3137771679633fb013d54dc2efc7e7a49
Andres Ladino <aladinoster@gmail.com>
2021-07-29 17:12:02 +0200

    👽 Updates to match operational tests

--- ensemble/component/vehicle.py
+++ ensemble/component/vehicle.py
@@ -21,8 +21,8 @@ from dataclasses import dataclass
 
 from ensemble.logic.subscriber import Subscriber
 from ensemble.tools import constants as ct
-from .dynamics import VehicleDynamic
-
+from ensemble.metaclass.stream import DataQuery
+from ensemble.metaclass.dynamics import AbsDynamics
 
 # ============================================================================
 # CLASS AND DEFINITIONS
@@ -31,47 +31,47 @@ from .dynamics import VehicleDynamic
 
 @dataclass
 class Vehicle(Subscriber):
-    """ Vehicle class defined for storing data on a single vehicle: 
+    """Vehicle class defined for storing data on a single vehicle:
 
-        You need a Publisher from where the vehicle is going to take data: 
+    You need a Publisher from where the vehicle is going to take data:
 
-        Args: 
-            request (Publisher): Parser or object publishing data
-        
-        Retunrns: 
-            vehicle (Vehicle): A Dataclass with vehicle parameters
+    Args:
+        request (Publisher): Parser or object publishing data
 
-        ============================  =================================
-        **Variable**                  **Description**
-        ----------------------------  ---------------------------------
-        ``abscissa``                    Current coordinate on y axis
-        ``acceleration``                Current acceleration
-        ``distance``                    Current distance traveled on link
-        ``elevation``                   Current elevation
-        ``lane``                        Current lane
-        ``link``                        Current road vehicle is traveling
-        ``ordinate``                    Current coordinate x axis
-        ``speed``                       Current speed
-        ``vehid``                       Vehicle id
-        ``vehtype``                     Vehicle class
-        ============================  =================================
+    Retunrns:
+        vehicle (Vehicle): A Dataclass with vehicle parameters
 
-        Example: 
-            This is one example on how to register a new vehicle ::
+    ============================  =================================
+    **Variable**                  **Description**
+    ----------------------------  ---------------------------------
+    ``abscissa``                    Current coordinate on y axis
+    ``acceleration``                Current acceleration
+    ``distance``                    Current distance traveled on link
+    ``elevation``                   Current elevation
+    ``lane``                        Current lane
+    ``link``                        Current road vehicle is traveling
+    ``ordinate``                    Current coordinate x axis
+    ``speed``                       Current speed
+    ``vehid``                       Vehicle id
+    ``vehtype``                     Vehicle class
+    ============================  =================================
 
-            >>> req = SimulatorRequest()
-            >>> veh = Vehicle(req)
-            >>> req.dispatch() # This will update vehicle data
+    Example:
+        This is one example on how to register a new vehicle ::
 
-        When having multiple vehicles please indicate the `vehid` before launching the dispatch method. This is because the vehicle object is looks for a vehicle id within the data. 
+        >>> req = SimulatorRequest()
+        >>> veh = Vehicle(req)
+        >>> req.dispatch() # This will update vehicle data
 
-        Example: 
-            This is one example on how to register two vehicles ::
+    When having multiple vehicles please indicate the `vehid` before launching the dispatch method. This is because the vehicle object is looks for a vehicle id within the data.
 
-            >>> req = SimulatorRequest()
-            >>> veh1 = Vehicle(req, vehid=0)
-            >>> veh2 = Vehicle(req, vehid=1)
-            >>> req.dispatch() # This will update vehicle data on both vehicles
+    Example:
+        This is one example on how to register two vehicles ::
+
+        >>> req = SimulatorRequest()
+        >>> veh1 = Vehicle(req, vehid=0)
+        >>> veh2 = Vehicle(req, vehid=1)
+        >>> req.dispatch() # This will update vehicle data on both vehicles
 
 
     """
@@ -89,17 +89,15 @@ class Vehicle(Subscriber):
     vehid: int = 0
     vehtype: str = ""
 
-    def __init__(self, request, **kwargs):
-        """ This initializer creates a Vehicle
-        """
+    def __init__(self, request: DataQuery, dynamics: AbsDynamics, **kwargs):
+        """This initializer creates a Vehicle"""
         # Undefined properties
         self.count = next(self.__class__.counter)
-        self.dynamic = VehicleDynamic()
+        self.dynamics = dynamics
         self.itinerary = []
 
         # Optional properties
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.update_no_request(**kwargs)
 
         super().__init__(request)
 
@@ -112,8 +110,7 @@ class Vehicle(Subscriber):
         return self.vehid == veh.vehid
 
     def update(self):
-        """ Updates data from publisher 
-        """
+        """Updates data from publisher"""
         dataveh = self._publisher.get_vehicle_properties(self.vehid)
         self.__dict__.update(**dataveh)
 
@@ -121,7 +118,16 @@ class Vehicle(Subscriber):
         if link not in getattr(self, "itinerary"):
             self.itinerary.append(link)
 
+    def update_no_request(self, **kwargs):
+        """Update vehicle data from specific keyword arguments"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
     @property
-    def x(self):
-        """Vehicle state vector (x,v,a)"""
+    def state(self) -> np.ndarray:
+        """Returns current state of the vehicle
+
+        Returns:
+            np.ndarray: [3d-array] @ k+1 [distance;speed;acceleration]
+        """
         return np.array((self.distance, self.speed, self.acceleration))
--- ensemble/component/vehiclelist.py
+++ ensemble/component/vehiclelist.py
@@ -26,21 +26,28 @@ from ensemble.logic.frozen_set import SortedFrozenSet
 
 
 class VehicleList(SortedFrozenSet):
-    """ Class defining a set of vehicles. This class is based on a sorted 
-        frozen set and supports multiple operations in between sets. You can define a list based on a simluator request and the list will update automatically via a single method. 
+    """Class defining a set of vehicles. This class is based on a sorted
+    frozen set and supports multiple operations in between sets. You can define a list based on a simluator request and the list will update automatically via a single method.
 
-        Args: 
-            request (Publisher): Publisher of information 
+    Args:
+        request (Publisher): Publisher of information
 
-        Example: 
-            Define a list of vehicles to trace the requests ::
-                >>> simrequest = SimulatorRequest()
-                >>> simrequest.query = one_vehicle_xml
-                >>> vl = VehicleList(simrequest)
-                >>> simrequest.query = second_vehicle_xml
-                >>> vl.update_list() # This updates manually
+    Example:
+        Define a list of vehicles to trace the requests ::
+            >>> simrequest = SimulatorRequest()
+            >>> simrequest.query = one_vehicle_xml
+            >>> vl = VehicleList(simrequest)
+            >>> simrequest.query = second_vehicle_xml
+            >>> vl.update_list() # This updates manually
 
-        The list could be eventually updated as an observer but for simplicity reasons it is kept like this. 
+    Example:
+        Define a list of vehicles to be used manually::
+            >>> simrequest = SimulatorRequest()
+            >>> vl = VehicleList(simrequest)
+            >>> v1 = Vehicle(simrequest, vehid=1)
+            >>> v2 = Vehicle(simrequest, vehid=2)
+            >>> vl.update_list(optional=[v1,v2]) # Adds vehicles to the list
+    The list could be eventually updated as an observer but for simplicity reasons it is kept like this.
     """
 
     def __init__(self, request):
@@ -48,49 +55,42 @@ class VehicleList(SortedFrozenSet):
         data = [Vehicle(request, **v) for v in request.get_vehicle_data()]
         super().__init__(data)
 
-    def update_list(self):
-        """ Update vehicle data according to an update in the request.
-        """
-        data = self + VehicleList(self._request)
+    def update_list(self, extra=[]):
+        """Update vehicle data according to an update in the request."""
+        data = self + VehicleList(self._request) + SortedFrozenSet(extra)
         self._items = data._items
 
     def _get_vehicles_attribute(self, attribute: str) -> pd.Series:
-        """ Retrieve list of parameters 
-        
-            Args: 
-                attribute (str): One of the vehicles attribute e.g. 'distance'
-            
-            Returns 
-                dataframe (series): Returns values for a set of vehicles 
+        """Retrieve list of parameters
+
+        Args:
+            attribute (str): One of the vehicles attribute e.g. 'distance'
+
+        Returns
+            dataframe (series): Returns values for a set of vehicles
         """
         return self._to_pandas()[attribute]
 
     @property
     def acceleration(self) -> pd.Series:
-        """
-            Returns all vehicle's accelerations 
-        """
+        """Returns all vehicle's accelerations"""
         return self._get_vehicles_attribute("acceleration")
 
     @property
     def speed(self) -> pd.Series:
-        """
-            Returns all vehicle's accelerations 
-        """
+        """Returns all vehicle's accelerations"""
         return self._get_vehicles_attribute("speed")
 
     @property
     def distance(self) -> pd.Series:
-        """
-            Returns all vehicle's accelerations 
-        """
+        """Returns all vehicle's accelerations"""
         return self._get_vehicles_attribute("distance")
 
     def _to_pandas(self) -> pd.DataFrame:
-        """ Transforms vehicle list into a pandas for rendering purposes 
-        
-            Returns: 
-                df (DataFrame): Returns a table with pandas data.
+        """Transforms vehicle list into a pandas for rendering purposes
+
+        Returns:
+            df (DataFrame): Returns a table with pandas data.
 
         """
         return pd.DataFrame([asdict(v) for v in self._items])
