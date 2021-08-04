993e33b9aa93569ee107f5f3832e8161b748a6e0
Andres Ladino <aladinoster@gmail.com>
2021-08-04 10:04:56 +0200

    🧪 Test dynamics

--- tests/dynamics/test_dynamics.py
+++ tests/dynamics/test_dynamics.py
@@ -11,6 +11,7 @@ Unit testing
 # STANDARD  IMPORTS
 # ============================================================================
 
+from matplotlib import pyplot as plt
 import pytest
 import numpy as np
 from numpy.testing import assert_array_equal, assert_almost_equal
@@ -53,4 +54,74 @@ def test_dynamics_truck_single_step():
         state,
         np.array([2.227778, 24.722222, 0.0]),
         decimal=6,
+    )
+
+
+def test_dynamics_truck_300_step():
+    t = TruckDynamics(vehid=0, x=0, a=0, v=25)
+    full_state = np.empty((3,))
+    state = np.empty((3,))
+    full_time = []
+    for time in np.arange(0, 30, 0.1):
+        state = t(np.array([]), np.array([0.5]))
+        full_state = np.vstack((full_state, state))
+        full_time.append(time)
+
+    f, a = plt.subplots(1, 3, figsize=(15, 5))
+    a[0].plot(full_time, full_state[1:, 1])
+    a[0].set_xlabel("Time [s]")
+    a[0].set_ylabel("Speed [m/s]")
+    a[0].grid(True)
+
+    a[1].plot(full_time, full_state[1:, 2])
+    a[1].set_xlabel("Time [s]")
+    a[1].set_ylabel("Acceleration [m/s2]")
+    a[1].grid(True)
+
+    a[2].plot(full_time, full_state[1:, 0])
+    a[2].set_xlabel("Time [s]")
+    a[2].set_ylabel("Position [m]")
+    a[2].grid(True)
+
+    # plt.show()
+
+    assert_almost_equal(
+        full_state[-1],
+        np.array([7.410787e02, 2.472222e01, 2.742790e-01]),
+        decimal=4,
+    )
+
+
+def test_dynamics_truck_300_step():
+    t = TruckDynamics(vehid=0, x=0, a=0, v=0)
+    full_state = np.empty((3,))
+    state = np.empty((3,))
+    full_time = []
+    for time in np.arange(0, 30, 0.1):
+        state = t(np.array([]), np.array([0.5]))
+        full_state = np.vstack((full_state, state))
+        full_time.append(time)
+
+    f, a = plt.subplots(1, 3, figsize=(15, 5))
+    a[0].plot(full_time, full_state[1:, 1])
+    a[0].set_xlabel("Time [s]")
+    a[0].set_ylabel("Speed [m/s]")
+    a[0].grid(True)
+
+    a[1].plot(full_time, full_state[1:, 2])
+    a[1].set_xlabel("Time [s]")
+    a[1].set_ylabel("Acceleration [m/s2]")
+    a[1].grid(True)
+
+    a[2].plot(full_time, full_state[1:, 0])
+    a[2].set_xlabel("Time [s]")
+    a[2].set_ylabel("Position [m]")
+    a[2].grid(True)
+
+    # plt.show()
+
+    assert_almost_equal(
+        full_state[-1],
+        np.array([201.1149, 13.3752, 0.5]),
+        decimal=4,
     )
