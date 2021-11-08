f541f8e5d58dc8de98dd4c6df1aa226105d11f80
Andres Ladino <aladinoster@gmail.com>
2021-11-08 11:20:34 +0100

    📈 Plot basic_tests

--- ensemble/control/operational/basic_test.py
+++ ensemble/control/operational/basic_test.py
@@ -3,6 +3,7 @@
 # ============================================================================
 
 from matplotlib.colors import LinearSegmentedColormap
+from networkx.readwrite.gml import escape
 import numpy as np
 
 # ============================================================================
@@ -47,7 +48,7 @@ def runtime_op_layer(initial_condition: np.ndarray, scenario: str = "platoon"):
             request,
             vehid=i,
             vehtype="PLT",
-            dyn_path=truck_path,
+            dyn_path=DEFAULT_TRUCK_PATH,
             distance=x0[0],
             abscissa=x0[0],
             speed=x0[1],
@@ -63,22 +64,84 @@ def runtime_op_layer(initial_condition: np.ndarray, scenario: str = "platoon"):
     ggc.cacc = cacc
 
     # Runtime evolution
-    sim_time = 60  # [s]
+    sim_time = 300  # [s]
 
     for t in range(sim_time):
+        print(t)
         ggc.apply_cacc(t)
+
+    return ggc
 
 
 if __name__ == "__main__":
-    import os
-
-    cacc_path = os.path.join(
-        os.getcwd(), "ensemble", "libs", "darwin", "OperationalDLL.dylib"
-    )
-    truck_path = os.path.join(
-        os.getcwd(), "ensemble", "libs", "darwin", "truckDynamics.dylib"
-    )
 
     # Initial condition
-    X0 = np.array([[40, 25], [10, 26]])
-    runtime_op_layer(X0)
+    X0 = np.array([[80, 25], [60, 26], [40, 25], [20, 10]])
+    ggc = runtime_op_layer(X0)
+
+    X = np.vstack([ggc[i].history_state[:, 0] for i in range(4)]).T
+    V = np.vstack([ggc[i].history_state[:, 1] for i in range(4)]).T
+    A = np.vstack([ggc[i].history_state[:, 2] for i in range(4)]).T
+    S = np.vstack(
+        [
+            ggc[i].history_state[:, 0] - ggc[i + 1].history_state[:, 0]
+            for i in range(3)
+        ]
+    ).T
+
+    ES = np.vstack(
+        [
+            ggc[i].history_reference[:, 2] * V[:, i] - S[:, i - 1]
+            for i in range(1, 4)
+        ]
+    ).T
+
+    EV = np.vstack(
+        [ggc[i].history_reference[:, 1] - V[:, i] for i in range(4)]
+    ).T
+
+    from matplotlib import pyplot as plt
+    import numpy as np
+
+    t = ggc[0].history_reference[:, 0]
+    fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+    ax[0].plot(t, X, label="Position [m]")
+    ax[1].plot(t, V, label="Speed [m/s]")
+    ax[2].plot(t, A, label="Acceleration [m/s2]")
+    ax[3].plot(t, S, label="Headway Space [m]")
+    [a.grid() for a in ax]
+    [a.set_xlabel("Time [s]") for a in ax]
+    [
+        a.set_ylabel(b)
+        for a, b in zip(
+            ax,
+            (
+                "Position $x_i[m]$",
+                "Speed $v_i[m/s]$",
+                "Acceleration $a_i[m/s^2]$",
+                "Space Headway $(x_{i-1}-x_i)[m]$",
+            ),
+        )
+    ]
+    plt.tight_layout()
+
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+    ax[0].plot(t, ES, label="Position [m]")
+    ax[1].plot(t, EV, label="Speed [m/s]")
+    [a.grid() for a in ax]
+    [a.set_xlabel("Time [s]") for a in ax]
+    [
+        a.set_ylabel(b)
+        for a, b in zip(
+            ax,
+            (
+                "Error Headway space \n "
+                + r"$ e_s = \tau_g *v_i - (x_{i-1}-x_{i}) - [m]$",
+                "Error Speed \n  $e_v = r_{v_i} - v_i - [m/s]$",
+            ),
+        )
+    ]
+    plt.tight_layout()
+    plt.show()
+
+    ggc
